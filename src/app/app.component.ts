01d8d0fe285df1388746d859856d2cad9a6de083
Joewings-jw <ngugijoseph830@gmail.com>
2021-05-27 16:48:10 +0300

    app container logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, OnDestroy, OnChanges} from '@angular/core';
+import { Breakpoints, BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular11Crm';
+export class AppComponent implements OnInit, OnChanges{
+  title = 'ng crm';
+  user: any = null;
+  mode = "side"
+  uiContent = "content"
+  progrssBarClass = "progress-bar";
+  isloading = true;
+
+  constructor() {
+    console.log(" constructor")
+
+    this.isloading = true;
+
+  }
+
+  ngOnChanges() {
+    console.log("ngOnChanges")
+  }
+
+
+  ngOnInit(): void {
+    console.log(" ngOnInit")
+  }
+
+
 }
