4a4a90f7d9ffa0937d839d531a552329b51b0dfe
Joewings-jw <ngugijoseph830@gmail.com>
2021-05-31 15:31:57 +0300

    module import for child paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard/dashboard.component';
 
+
 const routes: Routes = [
   {
     path: "dashboard",
     component: DashboardComponent,
   },
+  {
+    path: "customers",
+    loadChildren: () =>
+    import('./customer/customer.module').then(m => m.CustomerModule)
+  },
+
 ];
 
 @NgModule({
