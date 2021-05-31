3038c34d3e59cccaf6a05934092c9ab476825647
Joewings-jw <ngugijoseph830@gmail.com>
2021-05-31 15:22:50 +0300

    user model defined

---
+++ src/app/user_model/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    id: number;
+    token: string;
+    username: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    is_authenticated: boolean
+}
