c830e03e2b63a1a87d32566230c1811fd7902af0
Joewings-jw <ngugijoseph830@gmail.com>
2021-06-09 12:09:47 +0300

    lazy loading components

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,21 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { CustomerListComponent } from "../customer/customer_list/customer-list.component";
+import { CustomerFormComponent } from "../customer/customer_form/customer-form.component";
 import { CustomerService } from "./customer.service";
 import { MaterialModule } from "../shared/material.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent,
+    CustomerFormComponent,
+  ],
   imports: [
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild([
       { path: "", component: CustomerListComponent },
+      {
+        path: "new/",
+        component: CustomerFormComponent
+      },
+      {
+        path: "edit/:id",
+        component: CustomerFormComponent
+      }
     ])
   ],
   providers:[CustomerService],
-  exports: [CustomerListComponent]
+  exports: [CustomerListComponent,
+    CustomerFormComponent,
+  ]
 })
 export class CustomerModule { }
