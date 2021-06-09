d8070f22d76bcc8a9b6869857597cc1c965ab6ba
Joewings-jw <ngugijoseph830@gmail.com>
2021-06-09 12:15:59 +0300

    subscribed to observables

--- src/app/customer/customer_list/customer-list.component.ts
+++ src/app/customer/customer_list/customer-list.component.ts
@@ -4,12 +4,11 @@ import { CustomerService } from '../customer.service';
 
 import * as _ from 'lodash';
 
-import {MatDialog} from '@angular/material/dialog'
+import { MatFormFieldControl } from '@angular/material/form-field';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
-import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -27,9 +26,10 @@ export class CustomerListComponent implements OnInit {
     image_width: number = 30;
     image_margin: number = 2;
     show_image: boolean = false;
+    list_filter: any = {};
     error_message: string;
 
-    customers: Customer[] | any ;
+    customers: Customer[] | any;
     customer_list: Customer[]; //
     displayedColumns = ["avatar", "first_name", "last_name", "rewards", "email", "membership", "id"];
     dataSource: any = null;
@@ -46,34 +46,27 @@ export class CustomerListComponent implements OnInit {
     constructor(
         private service: CustomerService) {
     }
-
-
-    freshDataList(customers: Customer[]) {
-        this.customers = customers;
-
-        this.dataSource = new MatTableDataSource(this.customers);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-    }
-
-
+ 
     ngOnInit(): void {
         this.all_customers()
+       
     }
 
   
     all_customers (){
         this.service.all_customers().subscribe((customer)=>{
             this.customers = (customer.length > 0) ? [...customer] : null;
-            this.freshDataList(this.customers)
+            this.dataSource = new MatTableDataSource(this.customers);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
             console.log(this.customers)
         },
         err=> this.error_message = <any>err 
         );
     }
 
-   
-
+    
+    
 
   
 
